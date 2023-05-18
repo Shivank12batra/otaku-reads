db9b37ecb8fb65e567833d2135fa9168c4f1008b
Shivank12batra <Shivank56batra@gmail.com>
2023-05-18 13:32:59 +0530

    style: categories component styling done

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import './Category.css'
 
 const Category = () => {
+    const [categories, setCategories] = useState([])
+
+    const fetchData = async() => {
+        const res = await fetch('/api/categories')
+        const data = await res.json()
+        setCategories(data.categories)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
   return (
-    <div>Category</div>
+    <div className='category-container'>
+        <h1 className='category-header'>Categories</h1>
+        {categories.length > 0 ? <div className='categories'>
+            {categories.map(({_id, img, categoryName, description}) => {
+                return (
+                    <li key={_id} className='category-card'>
+                        <img src={img} alt={categoryName}/>
+                        <h2>{categoryName}</h2>
+                        <p>{description}</p>
+                    </li>
+                )
+            })}
+        </div> : null}
+    </div>
   )
 }
 
