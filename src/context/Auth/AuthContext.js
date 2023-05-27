c2c95aa7b802eebdd375f51a133bbaeb3b38f784
Shivank12batra <Shivank56batra@gmail.com>
2023-05-27 16:58:14 +0530

    style: product list made responsive

--- src/context/Auth/AuthContext.js
+++ src/context/Auth/AuthContext.js
@@ -1,17 +1,10 @@
 import {createContext, useContext, useState} from 'react';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({children}) => {
     const [token, setToken] = useState(localStorage?.getItem("token"));
     const [user, setUser] = useState(JSON.parse(localStorage?.getItem("user")));
-    // const [token, setToken] = useState()
-    // const [user, setUser] = useState()
-
-    console.log(token);
-    console.log(user);
 
     return (
         <AuthContext.Provider value={{token, setToken, user, setUser}}>
