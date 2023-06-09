cf36ca4fbecd394a1192c5c96f15cdc8629e3887
Shivank12batra <Shivank56batra@gmail.com>
2023-06-09 16:08:17 +0530

    chore: auth folder

--- src/context/Auth/AuthContext.js
+++ src/context/Auth/AuthContext.js
@@ -6,8 +6,6 @@ export const AuthProvider = ({children}) => {
     const [token, setToken] = useState(localStorage?.getItem("token"));
     const [user, setUser] = useState(JSON.parse(localStorage?.getItem("user")));
 
-    console.log(user)
-
     return (
         <AuthContext.Provider value={{token, setToken, user, setUser}}>
             {children}
